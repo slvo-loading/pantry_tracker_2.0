aa96fec2e8fa74ecd32f3e08a5aa418ffd6dc9c4
slvo-loading <sydneyvo098@gmail.com>
2024-08-03 23:18:29 -0700

    Normalize line endings

--- pantry_tracker_2.0/app/page.js
+++ pantry_tracker_2.0/app/page.js
@@ -1,8 +1,11 @@
 "use client";
 import Image from "next/image";
-import React, {useState, useEffect} from 'react';
-import { collection, addDoc, getDoc, querySnapshot, query, onSnapshot, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, {useState, useEffect,} from 'react';
+import { collection, addDoc, getDoc, query, onSnapshot, deleteDoc, doc, 
+  updateDoc, orderBy } from "firebase/firestore";
 import {db} from './firebase'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Home() {
   const [items, setItems] = useState([
@@ -10,7 +13,11 @@ export default function Home() {
     // {name:'Pineapple', price: 4},
     // {name:'Rice', price: 2}
   ]);
-  const[newItem, setNewItem] = useState({name: '', quantity: 1, exp_date:''})
+  const[newItem, setNewItem] = useState({name: '', quantity: 1, exp_date: null})
+  
+  const dateSelector = () =>{
+    const [date, setDate] = useState(newDate());
+  }
 
   // add item to database
   const addItem = async (e) => {
@@ -22,7 +29,7 @@ export default function Home() {
       exp_date: newItem.exp_date,
       quantity: newItem.quantity,
     })
-    setNewItem({name:'', quantity: 1, exp_date:''})
+    setNewItem({name:'', quantity: 1, exp_date: ''})
     }
   }
 
@@ -35,8 +42,11 @@ export default function Home() {
       querySnapshot.forEach((doc) => {
         itemsArr.push({...doc.data(), id:doc.id})
       })
+
+      itemsArr.sort((a, b) => a.exp_date.toDate() - b.exp_date.toDate());
       setItems(itemsArr)
       })
+      return unsubscribe
     }, [])
 
   //delete items from database
@@ -46,7 +56,7 @@ export default function Home() {
 
     const currentData = docSnap.data()
     const newQuantity = currentData.quantity - 1
-    if(newQuantity == '0'){
+    if(newQuantity == 0){
       await deleteDoc(doc(db, "items", id))
     } else {
       await updateDoc(docRef, {
@@ -69,7 +79,10 @@ export default function Home() {
 
  //check for expiring items
 
-
+ //user inputs date
+ const userDate = (date) => {
+  setNewItem({ ...newItem, exp_date: date})
+ }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -83,12 +96,26 @@ export default function Home() {
               className="col-span-3 p-3 border" 
               type="text" 
               placeholder='Enter Item' />
-            <input 
+            {/* <DatePicker
+              selected={newItem.exp_date}
+              onChange={userDate}
+              dateFormat="MM/dd/yyyy"
+              className="col-span-2 p-3 border"
+              placeholderText="Enter Expiration Date"/> */}
+            {/* <input 
               value={newItem.exp_date} 
               onChange={(e) => setNewItem({...newItem, exp_date:e.target.value})}
-              className="col-span-2 p-3 border mx-3" 
+              className="col-span-2 p-3 border" 
               type="text" 
-              placeholder='Enter Exp. Date' />
+              placeholder='Enter Exp. Date' /> */}
+            <div className="col-span-2 border h-11 ml-3 mr-3">
+              <DatePicker
+                selected={newItem.exp_date}
+                onChange={userDate}
+                dateFormat="MM/dd/yyyy"
+                className="block w-full h-11 p-3"
+                placeholderText="Enter Expiration Date"/> 
+            </div>
             <button 
               onClick={addItem}
               className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" 
@@ -102,7 +129,7 @@ export default function Home() {
                   <span>{item.quantity}</span>
                 </div>
                 <button onClick={() => additionalItem(item.id)} className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">+</button>
-                <button onClick={() => deleteItem(item.id)} className="p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
+                <button onClick={() => deleteItem(item.id)} className="p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">x</button>
               </li>
             ))}
           </ul>
