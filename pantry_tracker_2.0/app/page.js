6dc960c258ed6c5616efe00697336675b80e322e
slvo-loading <sydneyvo098@gmail.com>
2024-08-03 19:48:26 -0700

    added the add functionality

--- pantry_tracker_2.0/app/page.js
+++ pantry_tracker_2.0/app/page.js
@@ -1,15 +1,45 @@
-"use client"
+"use client";
 import Image from "next/image";
 import React, {useState, useEffect} from 'react';
+import { collection, addDoc, getDocs, querySnapshot, query, onSnapshot, deleteDoc } from "firebase/firestore";
+import {db} from './firebase'
 
 export default function Home() {
   const [items, setItems] = useState([
-    {name:'donut', amount: 3},
-    {name:'Pineapple', amount: 4},
-    {name:'Rice', amount: 2}
+    // {name:'donut', price: 3},
+    // {name:'Pineapple', price: 4},
+    // {name:'Rice', price: 2}
   ]);
-
+  const[newItem, setNewItem] = useState({name: '', price: ''})
   const [total, setTotal] = useState(0);
+
+  // add item to database
+  const addItem = async (e) => {
+    e.preventDefault()
+    if(newItem.name !== '' && newItem.price !== ''){
+      //setItems([...items,newItem]);
+      await addDoc(collection(db, "items"), {
+      name: newItem.name.trim(),
+      price: newItem.price,
+    })
+    setNewItem({name:'', price:''})
+    }
+  }
+
+  //read items from database
+  useEffect(() => {
+    const q = query(collection(db, "items"))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let itemsArr = []
+
+      querySnapshot.forEach((doc) => {
+        itemsArr.push({...doc.data(), id:doc.id})
+      })
+      setItems(itemsArr)
+      })
+    }, [])
+
+  //delete items from database
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -17,16 +47,29 @@ export default function Home() {
         <h1 className='text-4xl p-4 text-center'>Pantry Tracker</h1>
         <div className="bg-slate-800 p-4 rounded-lg">
           <form className="grid grid-cols-6 items-center text-black">
-            <input className="col-span-3 p-3 border" type="text" placeholder='Enter Item' />
-            <input className="col-span-2 p-3 border mx-3" type="text" placeholder='Enter Exp. Date' />
-            <button className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" type="submit">+</button>
+            <input 
+              value={newItem.name} 
+              onChange={(e) => setNewItem({ ...newItem, name:e.target.value })} 
+              className="col-span-3 p-3 border" 
+              type="text" 
+              placeholder='Enter Item' />
+            <input 
+              value={newItem.price} 
+              onChange={(e) => setNewItem({...newItem, price:e.target.value})}
+              className="col-span-2 p-3 border mx-3" 
+              type="text" 
+              placeholder='Enter Exp. Date' />
+            <button 
+              onClick={addItem}
+              className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" 
+              type="submit">+</button>
           </form>
           <ul>
             {items.map((item, id) => (
               <li key={id} className="my-4 w-full flex justify-between bg-slate-950">
                 <div className="p-4 w-full flex justify-between">
                   <span className='capitalize'>{item.name}</span>
-                  <span>{item.amount}</span>
+                  <span>{item.price}</span>
                 </div>
                 <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
               </li>
