5ca28bdb2202568934f11c95a7b2c86dd01cbf0a
slvo-loading <sydneyvo098@gmail.com>
2024-08-06 19:58:23 -0700

    Normalize line endings

--- pantry_tracker_2.0/app/components/Navbar.jsx
+++ pantry_tracker_2.0/app/components/Navbar.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+"use client";
+import React, {useState, useEffect} from 'react';
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+    const {user, googleSignIn, logOut} = UserAuth();
+    const [loading, setLoading] = useState(true);
+
+    const handleSignIn = async () => {
+        try {
+            await googleSignIn()
+        } catch {
+            console.log("error");
+        }
+    }
+
+    const handleSignOut = async () => {
+        try{
+            await logOut()
+        } catch {
+            console.log("error")
+        }
+    }
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            await new Promise((resolve) => setTimeout(resolve, 50));
+            setLoading(false)
+        }
+        checkAuthentication();
+    }, [user])
+
     return (
         <div className ='h-20 w-full border-b-2 flex items-center justify-between'>
+
             <ul className='flex'>
-                <li className='p-4 cursor-pointer hover:text-slate-500'>Pantry</li>
+                    <li className='p-4 cursor-pointer'>Pantry</li>
             </ul>
-            <ul className='flex'>
-                <li className='p-4 cursor-pointer hover:text-slate-500'>Login</li>
-                <li className='p-4 cursor-pointer hover:text-slate-500'>Sign Up</li>
-            </ul>
+
+            {loading ? null : !user ? (
+                <ul className='flex'>
+                    <li onClick={handleSignIn} className='p-4 cursor-pointer hover:text-slate-500'>Login</li>
+                    <li onClick={handleSignIn} className='p-4 cursor-pointer hover:text-slate-500'>Sign Up</li>
+                </ul>
+            ) : (
+                <ul className='flex'>
+                    <li className='p-4 cursor-pointer'>Welcome, {user.displayName}</li>
+                    <li onClick={handleSignOut} className='p-4 cursor-pointer hover:text-slate-500'>Sign Out</li>
+                </ul>
+            )}
         </div>
 
     )
--- pantry_tracker_2.0/app/context/AuthContext.js
+++ pantry_tracker_2.0/app/context/AuthContext.js
@@ -1,12 +1,33 @@
 "use client";
 import { useContext, createContext, useState, useEffect } from 'react';
+import { signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState('squidney');
+    const [user, setUser] = useState(null);
 
-    return (<AuthContext.Provider value={{user}}>{children}</AuthContext.Provider>);
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth, provider);
+    }
+
+    const logOut = () => {
+        signOut(auth, provider)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth,(currentUser) => {
+            setUser(currentUser)
+        })
+        return () => unsubscribe()
+    }, [user])
+
+
+    return (<AuthContext.Provider value={{user, googleSignIn, logOut}}>
+        {children}
+        </AuthContext.Provider>);
 
 }
 
